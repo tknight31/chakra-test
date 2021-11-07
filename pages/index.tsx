88d983f8dc04619b01f79095d9c955ec1f570b82
Tyler Knight <tknightx31@gmail.com>
2021-11-07 11:41:35 -0500

    initial Chakra testing

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { Container, Flex, VStack } from "@chakra-ui/react";
+import Cart from "../src/sections/cart";
+import Details from "../src/sections/details";
+
+export default function Home() {
+  return (
+    <Container maxW="container.xl" p="0">
+      <Flex h="100vh" py="20">
+        <Details />
+        <Cart />
+      </Flex>
+    </Container>
+  );
+}
