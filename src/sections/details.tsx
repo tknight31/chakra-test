64c3cbb57316589516fb642bd230e0bff2d579a9
Tyler Knight <tknightx31@gmail.com>
2021-11-07 11:41:51 -0500

    basic comps for form page

---
+++ src/sections/details.tsx
@@ -0,0 +1,43 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+  Heading,
+  Text,
+  SimpleGrid,
+  GridItem,
+} from "@chakra-ui/react";
+
+const Details = () => {
+  return (
+    <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start">
+      <VStack spacing={3} alignItems="flex-start">
+        <Heading size="2xl">Your Details</Heading>
+        <Text>If you already have an account, click here to login.</Text>
+      </VStack>
+      <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full">
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>First Name</FormLabel>
+            <Input placeholder="Dante" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>Last Name</FormLabel>
+            <Input placeholder="Freeman" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <FormControl>
+            <FormLabel>Email</FormLabel>
+            <Input placeholder="dfreezy@testing.com" />
+          </FormControl>
+        </GridItem>
+      </SimpleGrid>
+    </VStack>
+  );
+};
+
+export default Details;
