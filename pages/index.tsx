edb47ba1303d67a2da712852faaf0073f0136933
Tyler Knight <tknightx31@gmail.com>
2021-11-08 09:53:46 -0500

    mobile stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,15 @@ import Cart from "../src/sections/cart";
 import Details from "../src/sections/details";
 
 export default function Home() {
+  // for responsiveness. [<480px, 480px<768px, >768px]
+  // for responsiveness. { base: "column-reverse", md: "row" }
   return (
     <Container maxW="container.xl" p="0">
-      <Flex h="100vh" py="20">
+      <Flex
+        h={{ base: "row", md: "100vh" }}
+        py={[0, 10, 20]}
+        direction={{ base: "column-reverse", md: "row" }}
+      >
         <Details />
         <Cart />
       </Flex>
